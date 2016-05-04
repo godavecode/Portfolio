77d9cf289071e0b7f77b54395722dd00652ad541
David Coello <level9coding@gmail.com>
2016-05-04 18:30:28 -0500

    assignment1

---
+++ assignment1/index.php
@@ -0,0 +1,7 @@
+/**
+ * @Author: David Coello
+ * @Date:   2016-05-04 18:18:11
+ * @Last Modified by:   David Coello
+ * @Last Modified time: 2016-05-04 18:20:51
+ */
+<?php include_once("index.html"); ?>
